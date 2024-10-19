5e3b1c4045bd2dc8b767e48d6578168a651dae27
口x口 <92221534+kly777@users.noreply.github.com>
2024-10-19 18:43:24 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from "path"
+import autoprefixer from "autoprefixer"
+import cssnano from "cssnano";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(),
+        cssnano()
+      ],
+    },
+  },
+  resolve: {
+    // 设置文件./src路径为 @
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src')
+      }
+    ]
+  }
+})
