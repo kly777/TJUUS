63af69fdb24d9c2915617f56abd4b8da3e1e6d76
kly777 <92221534+kly777@users.noreply.github.com>
2024-12-25 11:40:06 +0000

    Deploying to dist from @ kly777/TJUUS@7cf1f10f675147fe624bf6b2df2f78a765733e32 🚀

---
+++ assets/About-DUQ3UC7s.js
@@ -0,0 +1,16 @@
+import{M as t}from"./index-BMSQkwth.js";import{d as e,P as o,Q as s,R as a,O as c,_ as r}from"./index-Lixa1wv7.js";const _=`# TJUUS
+
+## TJUUS 是一个以天津大学为组织中心的 MC 社区 
+
+**欢迎加入TJUUS！**
+
+这里是所有热爱《我的世界》（Minecraft）的同学们的聚集地，一个充满创造力、想象力和团队精神的虚拟世界。
+
+**加入我们，一起创造无限可能！**
+
+* **QQ群：**
+
+* **联系邮箱：**
+
+**期待你的加入！**
+`,d={class:"container sm:mx-10 lg:mx-30 mt-8"},m=["innerHTML"],i=e({__name:"About",setup(l){const n=t().render(_);return(u,f)=>(o(),s("div",d,[a("div",{innerHTML:c(n),class:"content .dark:text-white"},null,8,m)]))}}),U=r(i,[["__scopeId","data-v-40c89fdc"]]);export{U as default};
