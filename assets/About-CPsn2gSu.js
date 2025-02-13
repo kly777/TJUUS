7700255361586991b53443be6f2c5d68e4623b2f
kly777 <92221534+kly777@users.noreply.github.com>
2025-02-13 04:22:59 +0000

    Deploying to dist from @ kly777/TJUUS@ccf0b2764c8045363dd10248cc0f7fc4c90769ab 🚀

--- assets/About-CPsn2gSu.js
+++ assets/About-CPsn2gSu.js
@@ -1,4 +1,4 @@
-import{M as t}from"./index-CK0Q1hQj.js";import{d as e,M as o,N as s,Y as a,P as r,_ as c}from"./index-CEwLU1cq.js";const _=`# TJUUS
+import{M as t}from"./index-CK0Q1hQj.js";import{d as e,M as o,N as s,Y as a,P as r,_ as c}from"./index-pKyTdgqn.js";const _=`# TJUUS
 
 ## TJUUS 是一个以天津大学为组织中心的 MC 社区 
 
