1662005b8367873e2ab04df10318a2f7dfe478d7
kly777 <kly777777777@gmail.com>
2025-02-28 14:37:38 +0800

    build(vite): 添加 gzip 压缩插件
    - 添加 vite-plugin-compression 依赖压缩代码

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,22 @@ import cssnano from "cssnano";
 import UnoCSS from 'unocss/vite'
 import imagemin from 'vite-plugin-imagemin';
 import sitemapPlugin from 'vite-plugin-sitemap';
-
+import compression from 'vite-plugin-compression'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     UnoCSS(),
     imagemin(),
-    sitemapPlugin(),],
+    sitemapPlugin(),
+    compression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz'
+    })
+  ],
   css: {
     postcss: {
       plugins: [
