35a7a7ec5b750ff897c3aec4537e96b1b84c2d0e
kly777 <kly777777777@gmail.com>
2025-05-07 23:44:48 +0800

    refactor(components): 重构首页组件并移除 SPic 组件
    - 将 SPic 组件重命名为 PageBanner
    - 更新了 Home.vue 中的组件结构
    - 删除了 SPic.vue 文件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import "animate.css"
-import SPic from "./Home/SPic.vue"
+import PageBanner from "./Home/PageBanner.vue"
 import { NDivider } from "naive-ui"
 
 import Developing from "./Home/Developing.vue"
@@ -15,42 +15,42 @@ import Creeper from "./Home/Creeper.vue"
 
 <template>
     <!-- 页面顶部图片 -->
-    <SPic />
+    <PageBanner />
 
     <!-- 开发中 -->
-    <div class="intro sm:mx-10 md:mx-20 lg:mx-30 mx-0 w-85% py-8">
+    <section class="intro sm:mx-10 md:mx-20 lg:mx-30 mx-0 w-85% py-8">
         <Developing class="developing" />
-    </div>
+    </section>
 
     <n-divider :dashed="true"></n-divider>
 
     <!-- 信息卡片和右边的爬行者 -->
-    <div class="grid sc box-border justify-center items-center sm:mx-7 mx-9">
+    <section class="grid sc box-border justify-center items-center sm:mx-7 mx-9">
         <div class="grid-cols-1">
             <InfoCards class="row lg:w-80%" />
         </div>
         <div class="h-auto w-0 justify-self-start creeper">
             <Creeper class="md:h-400px md:w-400px lg:h-550px lg:w-550px sm:h-350px sm:w-300px w-250px h-260px" />
         </div>
-    </div>
+    </section>
 
     <!-- 关于我们 -->
-    <div class="intro sm:mx-10 md:mx-20 lg:mx-30 mx-0 w-80% py-8">
+    <section class="intro sm:mx-10 md:mx-20 lg:mx-30 mx-0 w-80% py-8">
 
         <SectionTitle class="row my" main="我们是" sub="TJUUS"></SectionTitle>
 
-    </div>
+    </section>
 
     <!-- 玻璃视图 -->
     <GlassSplitView />
 
     <!-- 伙伴 -->
-    <div class="intro sm:mx-10 md:mx-20 lg:mx-30 mx-5 w-80% py-8">
+    <section class="intro sm:mx-10 md:mx-20 lg:mx-30 mx-5 w-80% py-8">
         <SectionTitle class="row my" main="建设成果" sub="探索Minecraft的无限可能"></SectionTitle>
         <FeaturedItems class="row" />
         <SectionTitle class="row my-5" main="我们的伙伴" sub="联合"></SectionTitle>
         <Carousel class="row my-5" />
-    </div>
+    </section>
 
     <!-- 关注我们 -->
     <FollowUs class="mt" />
